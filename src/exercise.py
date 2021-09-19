6e91658acc8a1930d0dacb54c66cd97af8ef1b7f
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-19 04:05:06 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
-
+    count = 0
+    
+    while True:
+        num = int(input('Give a number:'))
+        
+        if num == 0:
+            break
+        
+        count += 1
+   print('Number of numbers: ' + str(count))
 if __name__ == '__main__':
     main()
